a0fc27c9a18a30d8c0caf9c0e906e222042b4c53
Gustavo Clausen <guguclausen@gmail.com>
2024-09-07 20:12:40 -0300

    Fixing Budget model fields

--- budget/models.py
+++ budget/models.py
@@ -1,12 +1,17 @@
+import uuid
 from django.db import models
 
 
 class Budget(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.CharField(max_length=250, null=False)
     email = models.EmailField(null=False)
     whatsapp_number = models.CharField(max_length=100, null=False)
     source_language = models.CharField(max_length=100, null=False)
     target_language = models.CharField(max_length=100, null=False)
+    price = models.FloatField(null=False)
+    total_words = models.IntegerField(null=False)
+    created_at = models.DateField(auto_now_add=True)
 
 
 class File(models.Model):
